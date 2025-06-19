1844ae149b1dae047be38082c5a4e4b646cced59
Sagar Chavan <Sagarchavan0061@gmail.com>
2025-06-19 17:27:44 +0530

    message

--- my-app/postcss.config.mjs
+++ my-app/postcss.config.mjs
@@ -1,9 +1,4 @@
 const config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
